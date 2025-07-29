15d1e1ece19f0e4c3e41cdecb3e44619812efe86
gimgyuwon <gimgyuwon2@gmail.com>
2025-07-29 16:14:09 -0700

    feat: Add padding top and overflow hidden prop

--- fe/src/components/layout/Wrapper.tsx
+++ fe/src/components/layout/Wrapper.tsx
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   max-width: 420px;
   width: 100%;
+  padding-top: 100px;
   margin: 0 auto;
   min-height: 100vh;
+  overflow: hidden;
 `;
 
 export default Wrapper;
