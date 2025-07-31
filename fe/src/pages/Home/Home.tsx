86a3ece6ab4ab36699b95bf7d4e2e73c898d7bf7
gimgyuwon <gimgyuwon2@gmail.com>
2025-07-30 20:29:30 -0700

    feat: Add version text in home

--- fe/src/pages/Home/Home.tsx
+++ fe/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import {
   StyledCenterTitle,
   StyledWobbleButton,
   StyledText,
+  StyledBox,
 } from '@styles/common.style';
 import { useNavigate } from 'react-router-dom';
 
@@ -17,6 +18,12 @@ const Home = () => {
     <>
       <StyledCenterTitle>남이 보는 내 모습은?</StyledCenterTitle>
       <StyledText size="md">친구들이 생각하는 나는 어떤 모습일까?</StyledText>
+      <StyledBox $paddingTB="1rem">
+        <StyledText size="md" weight="bold" fontFamily="TalkFont">
+          - 사대천왕 고등학교 편 -
+        </StyledText>
+      </StyledBox>
+
       <ImageRotator />
       <StyledWobbleButton onClick={handleClickStart}>
         시작하기
