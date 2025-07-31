fb510f389f5aa6a9a6ec1212cf0bb0ddf7307ab8
gimgyuwon <gimgyuwon2@gmail.com>
2025-07-30 22:33:59 -0700

    feat: Supplement url and serializer

--- be/core/urls.py
+++ be/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_result, post_vote
 
 urlpatterns = [
-    path('result', get_result, name='get_result'),
-    path('vote', post_vote, name='post_vote'),
+    path('result/', get_result, name='get_result'),
+    path('vote/', post_vote, name='post_vote'),
 ]
